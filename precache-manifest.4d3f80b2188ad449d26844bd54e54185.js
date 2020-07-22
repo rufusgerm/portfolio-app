868f31e46a151bdea31e20424b12301fc861393e
Jeremy Bergevin <jdbergevin@mac.com>
2020-07-22 13:06:54 -0700

    Updates

---
+++ precache-manifest.4d3f80b2188ad449d26844bd54e54185.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae70d3b42f5e0794d8ef524680f2ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d2d0dc94c3e6e94cb6",
+    "url": "/static/css/main.b24b8d19.chunk.css"
+  },
+  {
+    "revision": "9720d75a6ea6529590e3",
+    "url": "/static/js/2.d3a12667.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3a12667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d2d0dc94c3e6e94cb6",
+    "url": "/static/js/main.c5510ad9.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "964b9e83d5ab7c388f083d6a5a71de60",
+    "url": "/static/media/appt-app.964b9e83.png"
+  },
+  {
+    "revision": "7685cf843aa173185d9302dffb3fbc32",
+    "url": "/static/media/css3.7685cf84.svg"
+  },
+  {
+    "revision": "10fd71c3998818106aa0fa54cfb0cbf2",
+    "url": "/static/media/dash.10fd71c3.svg"
+  },
+  {
+    "revision": "ebbcc07234ab6aae72a0ee11b9ab572b",
+    "url": "/static/media/delivery-app.ebbcc072.png"
+  },
+  {
+    "revision": "eb1e38d308857200bb7bf30b4233f7bb",
+    "url": "/static/media/flask.eb1e38d3.svg"
+  },
+  {
+    "revision": "ab1dea8d93a49c2e42e884540303a1fc",
+    "url": "/static/media/github.ab1dea8d.svg"
+  },
+  {
+    "revision": "0d9b5b26b5ed89d8508c6377dc4241d0",
+    "url": "/static/media/heroku.0d9b5b26.svg"
+  },
+  {
+    "revision": "b7b69ef0b14132b029d6973f9efebd2c",
+    "url": "/static/media/html5.b7b69ef0.svg"
+  },
+  {
+    "revision": "021a09785600c677e75ea3b35b4dd7f6",
+    "url": "/static/media/inventory-app.021a0978.png"
+  },
+  {
+    "revision": "c995cc3555a6a197de1e7c3c61a8a21e",
+    "url": "/static/media/java.c995cc35.svg"
+  },
+  {
+    "revision": "1732674d4c74c782ceb014ccfc2e4390",
+    "url": "/static/media/javascript.1732674d.svg"
+  },
+  {
+    "revision": "2692441bbf1fe22c918ad9ebff98cab1",
+    "url": "/static/media/jeremy_final_dialogs_circle.2692441b.png"
+  },
+  {
+    "revision": "bad696d746c213256da7a40f88938135",
+    "url": "/static/media/linux.bad696d7.svg"
+  },
+  {
+    "revision": "851971b68e6609c4f584abf22b0c6b06",
+    "url": "/static/media/mac.851971b6.svg"
+  },
+  {
+    "revision": "80cfd3686e02cc3f1df13eaaf60ca15a",
+    "url": "/static/media/mysql.80cfd368.svg"
+  },
+  {
+    "revision": "6af68515c25b30e3c692e073b3f2a886",
+    "url": "/static/media/netbeans.6af68515.svg"
+  },
+  {
+    "revision": "335ecef2f667ee37bd533fd95e481c99",
+    "url": "/static/media/news-analyzer-app.335ecef2.png"
+  },
+  {
+    "revision": "82e3bbb5df1f687562b855a0cadf0f77",
+    "url": "/static/media/pycharm.82e3bbb5.svg"
+  },
+  {
+    "revision": "c095cd0328fb087b3d1e5fa1bfab74c0",
+    "url": "/static/media/python.c095cd03.svg"
+  },
+  {
+    "revision": "d69817cf30b9ba3447917345954194b7",
+    "url": "/static/media/react-app.d69817cf.png"
+  },
+  {
+    "revision": "d01d0f47e585c705f8305ecb99dada0e",
+    "url": "/static/media/react.d01d0f47.svg"
+  },
+  {
+    "revision": "9c93c19ba347d747439f3ff06d8e2e40",
+    "url": "/static/media/vscode.9c93c19b.svg"
+  },
+  {
+    "revision": "27f529ca4c2a5ddae7191c41b931ba08",
+    "url": "/static/media/windows.27f529ca.svg"
+  }
+]);
